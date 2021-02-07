f99f35b8c14cc8e3a2a70c1a65ee0bff830fea31
Moises Jimenez <moisesejimenezg@outlook.com>
2021-02-07 22:44:53 +0100

    Fix search for car in front

--- src/sensor_fusion.cpp
+++ src/sensor_fusion.cpp
@@ -1,14 +1,18 @@
 #include "sensor_fusion.h"
 
+#include "algorithm"
 #include "lane.h"
 
 void SensorFusion::Update(const std::vector<std::vector<double>>& raw_data)
 {
+    objects_.clear();
     for (const auto car_raw_data : raw_data)
     {
         objects_.emplace_back(
             Object{car_raw_data[3], car_raw_data[4], car_raw_data[5], car_raw_data[6]});
     }
+    std::sort(objects_.begin(), objects_.end(),
+              [](const auto& lhs, const auto& rhs) { return lhs.s < rhs.s; });
 }
 
 std::optional<Object> SensorFusion::GetObjectInFront(const CarState& ego) const
@@ -19,16 +23,10 @@ std::optional<Object> SensorFusion::GetObjectInFront(const CarState& ego) const
     {
         const Lane object_lane{object.d};
         if (object_lane.GetLaneId() == ego_lane.GetLaneId() && object.s > ego.s &&
-            object.s - ego.s <= 5)
+            object.s - ego.s <= 30)
         {
-            if (!result.has_value())
-            {
-                result = object;
-            }
-            else if (result.value().s < object.s)
-            {
-                result = object;
-            }
+            result = object;
+            break;
         }
     }
     return result;
